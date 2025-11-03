472348abd7273914f17d23761978b7cbc34774ed
Lulin Liu <93459201+lulinliu@users.noreply.github.com>
2025-11-03 01:12:28 -0600

    Update experiment name in run1.sh

--- run1.sh
+++ run1.sh
@@ -24,4 +24,4 @@ torchrun \
   --master_port="${MASTER_PORT}" \
   -m scripts.train \
   --config=cosmos_predict2/configs/base/config.py \
-  -- experiment=predict2_video2world_lora_training_2b_1030nuo_10k
+  -- experiment=predict2_video2world_lora_training_14b_1030nuo_14b
